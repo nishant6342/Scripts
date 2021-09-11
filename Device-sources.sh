11dbf3c21fdfa13f66d30e98611eeb68ec715d4b
“Patriot-06” <mbmc172@gmail.com>
2021-09-11 16:57:54 +0000

    Scripts: Add Device-sources script

---
+++ Device-sources.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Script for syncing my Device source's
+
+#Go to source dir
+cd bin/scripts/...!
+
+echo -e "\Cloning DT\n"
+git clone https://github.com/Patriot-06/device_realme_x3 device/realme/x3
+
+echo -e "\Cloning VT\n"
+git clone https://github.com/Patriot-06/vendor-realme-x3 vendor/realme/x3
+
+echo -e "\Cloning KT\n"
+git clone https://github.com/Patriot-06/kernel_realme_x3 kernel/realme/x3
+
+echo -e "\Cloning Device source's done\n"
